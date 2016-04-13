3e681b722fe1c4a207883b4875729e2af25ae90b
Jennifer <jennifer.dunne.co@gmail.com>
2016-04-13 16:18:37 -0600

    changed to gradient boost model, added grid search

--- code/classify_genres.py
+++ code/classify_genres.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics
+import pickle
 
 def build_dataframes():
     '''
@@ -70,8 +71,10 @@ def transform_target(df):
     '''
     Given a DataFrame (df) with the string column 'genre_vec', return a dataframe
     with the new column 'target' that has an array of boolean integers for that value.
+    Then create a column called 'abr_targ' that has the abbreviated target array.
     '''
     df['target'] = df['genre_vec'].apply(make_bool_list)
+    df['abr_targ'] = df['target'].apply(abbreviate_targ)
     return df
 
 def make_target_matrix(target):
@@ -82,13 +85,6 @@ def make_target_matrix(target):
     num_cols = len(target[0])
     mat = np.matrix()
 
-def add_abr_targ(df):
-    '''
-    Given a dataframe with a column 'target', create a column 'abr_targ' that will
-    abbreviate the target genres: remove "crime", "history", "music", and "sport",
-    since these are more plot elements than true genres.
-    '''
-    df['abr_targ'] = df['target'].apply(abbreviate_targ)
 
 def create_sub_frame(df, n=500):
     '''
@@ -99,26 +95,91 @@ def create_sub_frame(df, n=500):
     return df[df['wd_ct'] > n]
 
 
+def iterate_grid(features, target):
+    '''
+    For every column in the target matrix, build a grid search to determine the
+    best parameters for predicting that target. Combine all of the models in a
+    list of models. Return the list of models.
+    '''
+    genre_list = ['action', 'animation', 'comedy', 'drama', 'family', 'fantasy', \
+    'horror', 'musical', 'mystery', 'romance', 'sci-fi', 'thriller', 'war', 'western']
+    model_list = []
+    for i in xrange(len(target[0])):
+        model = build_grid(features, target[:,i], genre_list[i])
+        model_list.append(model)
+    return model_list
+
+def build_grid(features, target, genre):
+    '''
+    Build an individual grid search across varying parameters for a single genre.
+    Return the best model.
+    '''
+    grid = GridSearchCV(GradientBoostingClassifier, {n_estimators: [100, 300, 500, 700], \
+    max_depth: [3,4,5,6,7]}, n_jobs=-1, random_state=42)
+    filename = '../data/grid_' + genre + '.txt'
+    with open(filename) as f:
+        f.write("The best estimator for " + genre + " was:\n")
+        f.write(grid.best_params_)
+        f.write("\nAnd a score of: {}".format(grid.best_score_))
+    return grid.best_estimator_
+
+
+def test_grid(model_list, features, target):
+    '''
+    Given a list of models for each genre, run the features through the models to
+    predict target labels, and compare the predictions to the true target labels.
+    '''
+    ypred_mat = np.empty(len(target), len(target[0]))
+    for i in xrange(len(target[0])):
+        model = model_list[i]
+        ypred = model.predict_proba(features)
+        for j, prob in enumerate(ypred):
+            ypred_mat[j,i] = prob
+    with open('../data/grid_abbr_500.txt','w') as f:
+        f.write("Model rounded by .25\n")
+        yrd = round_by(ypred_mat, .25)
+        f.write( metrics.classification_report(target, yrd) )
+        f.write("\nModel rounded by .3\n")
+        yrd = round_by(ypred_mat, .3)
+        f.write( metrics.classification_report(target, yrd) )
+        f.write("\nModel rounded by .2\n")
+        yrd = round_by(ypred_mat, .2)
+        f.write( metrics.classification_report(target, yrd) )
+        f.write("\nModel rounded by .4\n")
+        yrd = round_by(ypred_mat, .4)
+        f.write( metrics.classification_report(target, yrd) )
+
+
 def build_multi_class(features, target):
     '''
     Initialize a multi-label multi-class classifier, then fit it.
     '''
     print "Building model..."
-    model = OneVsRestClassifier(RandomForestClassifier(n_estimators = 500, n_jobs=-1, random_state=42), n_jobs=-1)
-    #model.multilabel_ = True
-    print "Training model..."
+    model = OneVsRestClassifier(GradientBoostingClassifier(n_estimators = 500, random_state=42), n_jobs=-1)
+    print "Training model...\n(This may take a while)"
     model.fit(features, target)
     return model
 
 def test_multi_class(model, features, target):
     '''
     Given a multi-label multi-class classifier, test features, and a test target,
-    score how well it does.
+    score how well it does. Write the scores to a file.
     '''
-    print "Creating predictions..."
-    ypred = model.predict_proba(features)
-    ypred = round_by(ypred, .25)
-    print metrics.classification_report(target, ypred)
+    with open('../data/gboost_abbr_500.txt','w') as f:
+        f.write("Creating predictions...\n")
+        ypred = model.predict_proba(features)
+        f.write("Model rounded by .25\n")
+        yrd = round_by(ypred, .25)
+        f.write( metrics.classification_report(target, yrd) )
+        f.write("\nModel rounded by .3\n")
+        yrd = round_by(ypred, .3)
+        f.write( metrics.classification_report(target, yrd) )
+        f.write("\nModel rounded by .2\n")
+        yrd = round_by(ypred, .2)
+        f.write( metrics.classification_report(target, yrd) )
+        f.write("\nModel rounded by .4\n")
+        yrd = round_by(ypred, .4)
+        f.write( metrics.classification_report(target, yrd) )
 
 def round_by(predictions, n):
     '''
@@ -142,7 +203,7 @@ def test_train(df):
     return matrices of features and targets for training and testing
     '''
     print "Dividing the data..."
-    target = df['target']
+    target = df['abr_targ']
     train = df['doc_vec_arr']
     Xtr, Xt, ytr, yt = train_test_split(train, target, test_size=.3, random_state=42)
     Xtr = pd.Series(Xtr.values)
@@ -167,21 +228,26 @@ def primary(df_movies, df_reviews):
     result = pd.merge(df_reviews, df_movies, how='left', on='title_url')
     result = transform_target(result)
     print "Creating subframes..."
-    df_all_150 = create_sub_frame(result, 150)
-    df_all_200 = create_sub_frame(result, 200)
+    #df_all_150 = create_sub_frame(result, 150)
+    #df_all_200 = create_sub_frame(result, 200)
     df_all_500 = create_sub_frame(result, 500)
-    Xtr150, Xt150, ytr150, yt150 = test_train(df_all_150)
-    Xtr200, Xt200, ytr200, yt200 = test_train(df_all_200)
+    #Xtr150, Xt150, ytr150, yt150 = test_train(df_all_150)
+    #Xtr200, Xt200, ytr200, yt200 = test_train(df_all_200)
     Xtr500, Xt500, ytr500, yt500 = test_train(df_all_500)
-    print "For reviews of 500 words or more..."
-    model500 = build_multi_class(Xtr500, ytr500)
-    test_multi_class(model500, Xt500, yt500)
-    print "For reviews of 200 words or more..."
-    model200 = build_multi_class(Xtr200, ytr200)
-    test_multi_class(model200, Xt200, yt200)
-    print "For reviews of 150 words or more..."
-    model150 = build_multi_class(Xtr150, ytr150)
-    test_multi_class(model150, Xt150, yt150)
+    #Xtrall, Xtall, ytrall, ytall = test_train(result)
+    #print "For reviews of 500 words or more..."
+    #model500 = build_multi_class(Xtr500, ytr500)
+    #test_multi_class(model500, Xt500, yt500)
+    #print "For reviews of 200 words or more..."
+    #model200 = build_multi_class(Xtr200, ytr200)
+    #test_multi_class(model200, Xt200, yt200)
+    #print "For reviews of 150 words or more..."
+    #model150 = build_multi_class(Xtr150, ytr150)
+    #test_multi_class(model150, Xt150, yt150)
+    #modelall = build_multi_class(Xtrall, ytrall)
+    #test_multi_class(modelall, Xtall, ytall)
+    model_list = iterate_grid(Xtr500, ytr500)
+    test_grid(model_list, Xt500, yt500)
 
 if __name__ == '__main__':
     df_movies, df_reviews = build_dataframes()
